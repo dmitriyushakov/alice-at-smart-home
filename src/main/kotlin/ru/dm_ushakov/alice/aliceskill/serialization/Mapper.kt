4bd78426c4d43862995b78e740f8a7a1af0205b2
Dmitrii Ushakov <dmitrii.ushakov@netcracker.com>
2022-09-06 23:39:57 +0400

    YAML Jackson ObjectMapper

---
+++ src/main/kotlin/ru/dm_ushakov/alice/aliceskill/serialization/Mapper.kt
@@ -0,0 +1,30 @@
+package ru.dm_ushakov.alice.aliceskill.serialization
+
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import ru.dm_ushakov.alice.aliceskill.capabilities.DeviceCapability
+import ru.dm_ushakov.alice.aliceskill.devices.BasicDevice
+import ru.dm_ushakov.alice.aliceskill.devices.Device
+import ru.dm_ushakov.alice.aliceskill.properties.DeviceProperty
+
+private fun createSkillModule(): Module {
+    val skillModule = SimpleModule()
+    skillModule.addAbstractTypeMapping(Device::class.java, BasicDevice::class.java)
+    skillModule.addDeserializer(DeviceCapability::class.java, DeviceCapabilityDeserializer())
+    skillModule.addDeserializer(DeviceProperty::class.java, DevicePropertyDeserializer())
+
+    return skillModule
+}
+
+private fun createYamlObjectMapper(): ObjectMapper {
+    val mapper = ObjectMapper(YAMLFactory())
+    mapper.findAndRegisterModules()
+    mapper.registerModule(skillModule)
+
+    return mapper
+}
+
+val skillModule = createSkillModule()
+val yamlObjectMapper = createYamlObjectMapper()
