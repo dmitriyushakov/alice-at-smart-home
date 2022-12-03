3d32a8a84483cd5c1e5ec06b99179f07ae4c4037
Dmitriy Ushakov <dmitryushakov1@yandex.ru>
2022-12-03 13:51:01 +0400

    componentName and entityType properties for custom entities.

--- src/main/kotlin/ru/dm_ushakov/alice/aliceskill/config/BasicCustomEntity.kt
+++ src/main/kotlin/ru/dm_ushakov/alice/aliceskill/config/BasicCustomEntity.kt
@@ -1,4 +1,7 @@
 package ru.dm_ushakov.alice.aliceskill.config
 
 abstract class BasicCustomEntity(
-    override val entityId: String): CustomEntity
+    override val entityId: String): CustomEntity {
+    override val componentName by lazy { super.componentName }
+    override val entityType by lazy { super.entityType }
+}
--- src/main/kotlin/ru/dm_ushakov/alice/aliceskill/config/CustomEntity.kt
+++ src/main/kotlin/ru/dm_ushakov/alice/aliceskill/config/CustomEntity.kt
@@ -1,7 +1,16 @@
 package ru.dm_ushakov.alice.aliceskill.config
 
+import com.fasterxml.jackson.annotation.JsonGetter
+import ru.dm_ushakov.alice.aliceskill.annotation.ComponentName
+import ru.dm_ushakov.alice.aliceskill.annotation.EntityType
 import ru.dm_ushakov.alice.aliceskill.devices.Lifecycle
 
 interface CustomEntity: Lifecycle{
     val entityId: String
+
+    @get:JsonGetter("component")
+    val componentName: String? get() = this::class.annotations.firstNotNullOfOrNull { it as? ComponentName }?.name
+
+    @get:JsonGetter("type")
+    val entityType: String? get() = this::class.annotations.firstNotNullOfOrNull { it as? EntityType }?.typeName
 }
